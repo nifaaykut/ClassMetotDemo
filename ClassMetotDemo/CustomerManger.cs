f9cf0dc1e91a2c196efc93103f6084ccf3a8154a
Ümit-Danone <mit-Danone@UMIT>
2023-05-09 12:32:33 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    internal class CustomerManger
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Müşteri Eklendi : "+ customer.Name );
+        }
+        public void Update(Customer customer) 
+        {
+            Console.WriteLine("Müşteri Adresi güncellendi : "+ customer.Address );
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.Name = "Test";
+            customer.Address = "İstanbul";
+
+            CustomerManger customerManger = new CustomerManger();
+            customerManger.Add(customer);
+            customerManger.Update(customer);
+        }
+    }
+}
